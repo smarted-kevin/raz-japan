2574239ceaddbf9718c80d40f869eefdbceb8f79
smarted-kevin <kevin@smarted.jp>
2025-10-20 09:29:30 +0900

    test to fix deployment issues to vercel

--- src/app/dashboard/admin/_actions/course.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { fetchMutation, fetchQuery } from "convex/nextjs";
-import type { NewCourseForm } from "./schemas";
-import { api } from "../../../../../convex/_generated/api";
-
-export async function addCourse(formData: NewCourseForm) {
-  return "Hello World";
-}
